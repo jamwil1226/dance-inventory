2f53906baf9614d0d00f24ca9e6f8df99e6b0447
Jamie Williams <jamwil1226@yahoo.com>
2022-01-19 20:46:19 -0500

    redux

---
+++ client/build/precache-manifest.0356b169009dc12a5f3d2a4413b6968b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd30055744c6c5732c4080d7f74cb0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2b721f627cc05d8e4c",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "62e44e508b436bb96a73",
+    "url": "/static/js/2.055edb18.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.055edb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2b721f627cc05d8e4c",
+    "url": "/static/js/main.f5865441.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
